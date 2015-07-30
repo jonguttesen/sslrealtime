7233e5935ab95ba3140fc1dd17fda22488bb5e49
jonguttesen <jonguttesen@users.noreply.github.com>
2015-07-30 12:49:07 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var net = require('net'),
+    JsonSocket = require('json-socket');
+    
+var port = 9838;
+var server = net.createServer();
+server.listen(port);
+server.on('connection', function(socket) { //This is a standard net.Socket
+    socket = new JsonSocket(socket); //Now we've decorated the net.Socket to be a JsonSocket
+    socket.on('message', function(message) {
+    	console.log( message.a + ": " + message.b );
+        //var result = message.a + message.b;
+        ///socket.sendEndMessage({result: result});
+    });
+});
